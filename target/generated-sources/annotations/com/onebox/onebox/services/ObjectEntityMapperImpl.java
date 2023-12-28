52e731fcd56a2a68005612131e6ab40bbe0e1283
pranav.bk <pranavbk.dev@gmail.com>
2023-12-28 19:24:31 +0530

    removed target folder from gitignore

---
+++ target/generated-sources/annotations/com/onebox/onebox/services/ObjectEntityMapperImpl.java
@@ -0,0 +1,21 @@
+package com.onebox.onebox.services;
+
+import com.onebox.entityobjects.BaseEntity;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-12-28T19:07:33+0530",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Private Build)"
+)
+@Component
+public class ObjectEntityMapperImpl implements ObjectEntityMapper {
+
+    @Override
+    public void updateEntityFromDto(BaseEntity taskDto, BaseEntity task) {
+        if ( taskDto == null ) {
+            return;
+        }
+    }
+}
