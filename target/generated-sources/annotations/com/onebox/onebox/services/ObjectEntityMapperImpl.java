d96260211c65834aed1a3eca23bc791dcc419eb8
pranav.bk <pranavbk.dev@gmail.com>
2023-12-29 13:12:24 +0530

    changed to jre17 for deployment

--- target/generated-sources/annotations/com/onebox/onebox/services/ObjectEntityMapperImpl.java
+++ target/generated-sources/annotations/com/onebox/onebox/services/ObjectEntityMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-28T19:07:33+0530",
+    date = "2023-12-29T13:10:36+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Private Build)"
 )
 @Component
